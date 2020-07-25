b7c97e80813af73193a75e1f7285cdd4bb31ff07
Kauê Murakami <kauetmurakami@gmail.com>
2020-07-25 18:35:01 -0300

    ajustando textos

--- lib/app/modules/landing/landing_page.dart
+++ lib/app/modules/landing/landing_page.dart
@@ -47,7 +47,7 @@ class LandingPage extends GetView<LandingController> {
       ),
     ),
     Container(
-      color: Color(0xffD8D8D8),
+      color: Colors.white,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisSize: MainAxisSize.max,
@@ -63,14 +63,14 @@ class LandingPage extends GetView<LandingController> {
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
                   "Sabemos que todos estão fazendo sua parte nesse delicado momento.",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
+                  style: TextStyle(color: Colors.deepPurple, fontSize: 24),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
                   "Confiamos em você e agradecemos por isso.",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
+                  style: TextStyle(color: Colors.purple, fontSize: 24),
                 ),
               ),
               Icon(
@@ -82,7 +82,7 @@ class LandingPage extends GetView<LandingController> {
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
                     'Já que você está fazendo papel, agora vamos falar um pouco do nosso.',
-                    style: TextStyle(color: Colors.white, fontSize: 24),
+                    style: TextStyle(color: Colors.deepPurple, fontSize: 24),
                   )),
             ],
           ),
@@ -106,21 +106,21 @@ class LandingPage extends GetView<LandingController> {
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
                   "Nosso objetivo é trazer a normalidade às nossas vidas de forma mais rápida !",
-                  style: TextStyle(color: Colors.black, fontSize: 24),
+                  style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
                   "Devolvendo a chance de compartilhar momentos com quem você ama, lado a lado.",
-                  style: TextStyle(color: Colors.black, fontSize: 24),
+                  style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
                   "Valorizando aquilo que o dinheiro não pode comprar, o tempo.",
-                  style: TextStyle(color: Colors.black, fontSize: 24),
+                  style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
               ),
             ],
@@ -145,21 +145,21 @@ class LandingPage extends GetView<LandingController> {
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
                   "Mas como?",
-                  style: TextStyle(color: Colors.black, fontSize: 24),
+                  style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  "A partir de uma base de dados, esses coletados pelo nosso app, para mapear cada indivídiuo do Brasil.",
-                  style: TextStyle(color: Colors.black, fontSize: 24),
+                  "A partir de uma base de dados governamental, juntamente com os dados que serão coletados pelo nosso app",
+                  style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  "A outra, fontes seguras de pesquisas, governamentais, estudos sobre a disseminação do vírus e próximos surtos.",
-                  style: TextStyle(color: Colors.black, fontSize: 24),
+                  "Com isso poderemos definir prioridades para cada cidade e, consequentemente, estados brasileiros.",
+                  style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
               ),
             ],
@@ -181,37 +181,32 @@ class LandingPage extends GetView<LandingController> {
           Padding(
             padding: EdgeInsets.all(20.0),
           ),
-          Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "A partir de dados coletados pelo app e por órgãos confiáveis.",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
+          SingleChildScrollView(
+                      child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Iremos definir uma logística para a distribuição das vacinas através desses parâmetros.",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Iremos definir uma logistica para as vacinas através desses parâmetros.",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Distribuindo com eficiência e precisão em locais prioritários.",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Distribuindo com eficiência e precisão em locais prioritários",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Isso significa poupar tempo na distribuição da vacina, salvando vidas, e ao mesmo tempo, poupando dinheiro dos cofres públicos, ajudando também na recuperação econômica.",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Isso significa poupar tempo na distribuição da vacina e dinheiro dos cofres públicos.",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
@@ -235,21 +230,21 @@ class LandingPage extends GetView<LandingController> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  "Com a chegada rápida da vacina, podemos poupar milhares de vidas.",
+                  "Com a chegada rápida e precisa da vacina, ainda podemos poupar milhares de vidas.",
                   style: TextStyle(color: Colors.black, fontSize: 24),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  "Muitas pessoas do grupo de risco ainda não foram contaminadas.",
+                  "E mesmo com a descoberta de várias potenciais vácinas, o vírus não irá parar até que todos estejam imunizados.",
                   style: TextStyle(color: Colors.black, fontSize: 24),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  "Nosso objetivo é alcança-los com a vacina antes disso !",
+                  "E sabemos que muitas pessoas do grupo de risco, ainda não contrariam o vírus, essas devem ter prioridade na imunização.",
                   style: TextStyle(color: Colors.black, fontSize: 24),
                 ),
               ),
