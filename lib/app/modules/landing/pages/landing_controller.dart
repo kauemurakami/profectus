1aa3011681615f37e21184bcaff6603c0738893f
Lucsbezerra <lucasmingoni5@gmail.com>
2020-08-08 18:30:28 -0300

    telas e imagens

---
+++ lib/app/modules/landing/pages/landing_controller.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:meta/meta.dart';
+import 'package:vitalis/app/data/repository/user_repository.dart';
+
+class LandingController extends GetxController {
+
+  final UserRepository repository;
+  LandingController({@required this.repository}) : assert(repository != null);
+
+  final _page = 0.obs;
+  get page => this._page.value;
+  set page(value) => this._page.value = value;
+  
+  UpdateType updateType;
+
+}
